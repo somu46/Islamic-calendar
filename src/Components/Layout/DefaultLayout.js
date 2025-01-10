5c3619dbecf695aeb280302922bf405a2b40b1f3
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2025-01-10 23:57:15 +0530

    `Refactored layout and navigation components for improved responsiveness and user experience.`

--- src/Components/Layout/DefaultLayout.js
+++ src/Components/Layout/DefaultLayout.js
@@ -2,38 +2,29 @@ import React, { useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import Navbar from '../Navbar/Navbar';
 
-
-
-
 const DefaultLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="bg-boxdark-2 text-bodydark">
-       <Navbar/>
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
-      <div className="flex h-screen ">
-        {/* <!-- ===== Sidebar Start ===== --> */}
+    <div className="bg-boxdark-2 text-bodydark h-screen flex flex-col">
+      {/* Navbar */}
+      <Navbar setSidebarOpen={setSidebarOpen} />
+
+      {/* Page Wrapper */}
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar */}
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
 
-        {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Start ===== --> */}
-       
-          {/* <!-- ===== Header End ===== --> */}
-
-          {/* <!-- ===== Main Content Start ===== --> */}
+        {/* Content Area */}
+        <div className="relative flex flex-1 flex-col overflow-y-auto bg-gray-100">
+          {/* Main Content */}
           <main>
             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
               {children}
             </div>
           </main>
-          {/* <!-- ===== Main Content End ===== --> */}
         </div>
-        {/* <!-- ===== Content Area End ===== --> */}
       </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
     </div>
   );
 };
